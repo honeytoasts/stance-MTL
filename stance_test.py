8b4094a914734efb0718a7a1167def9a6d875bef
honeytoasts <jeremy0923@hotmail.com>
2021-02-18 11:29:10 +0800

    finish stance_test, evaluate test dataset

--- stance_test.py
+++ stance_test.py
@@ -1,4 +1,5 @@
 # built-in module
+import argparse
 import os
 import pickle
 import random
@@ -8,164 +9,155 @@ import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
-from sklearn.metrics import f1_score
+from torch.utils.tensorboard import SummaryWriter
+from sklearn.model_selection import train_test_split
 from tqdm import tqdm
-import sklearn
+import tensorboard
 
 # self-made module
 import util
 
-# parameter and model path setting
-experiment_no = 43
-epoch = 19
-model_path = f'model/{experiment_no}/'
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Evaluate the stance-detection model'
+    )
 
-# load config, tokenizer, embedding
-config = util.config.BaseConfig()
-config = config.load(f'{model_path}/config.json')
+    # add argument to argparser
+    parser.add_argument('--experiment_no',
+                        default='1',
+                        type=str)
+    parser.add_argument('--epoch',
+                        default='1',
+                        type=str)
 
-tokenizer = util.tokenizer.WordPunctTokenizer(config)
-tokenizer.load(f'{model_path}/tokenizer.pickle')
+    return parser.parse_args()
 
-embedding = util.embedding.BaseEmbedding()
-embedding.load(f'{model_path}/embedding.pickle')
+def main():
+    # pylint: disable=no-member
 
-# initialize random seed and device
-device = torch.device('cpu')
-os.environ['PYTHONHASHSEED'] = str(config.random_seed)
-random.seed(config.random_seed)
-np.random.seed(config.random_seed)
-torch.manual_seed(config.random_seed)
+    # get experiment_no and epoch
+    args = parse_args()
+    experiment_no, epoch = args.experiment_no, args.epoch
+    model_path = f'model/{experiment_no}'
 
-if torch.cuda.is_available():
-    device = torch.device('cuda:0')
-    torch.cuda.manual_seed(config.random_seed)
-    # torch.cuda.manual_seed_all(config.random_seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # load config, tokenizer, embedding
+    config = util.config.load(f'{model_path}/config.json')
 
-# load data
-if config.stance_dataset == 'semeval2016':
-    data_df = util.data.load_dataset('semeval2016_test')
-elif config.stance_dataset == 'fnc-1':
-    data_df = util.data.load_dataset('fnc_test')
+    if config.tokenizer == 'BaseTokenizer':
+        tokenizer = util.tokenizer.BaseTokenizer(config)
+    elif config.tokenizer == 'WordPunctTokenizer':
+        tokenizer = util.tokenizer.WordPunctTokenizer(config)
+    elif config.tokenizer == 'TweetTokenizer':
+        tokenizer = util.tokenizer.TweetTokenizer(config)
+    tokenizer.load(f'{model_path}/tokenizer.pickle')
 
-# content encode to id
-print('content encode --')
-data_df['target_encode'] = \
-    tokenizer.encode(data_df['target'].tolist())
-data_df['claim_encode'] = \
-    tokenizer.encode(data_df['claim'].tolist())
+    embedding = util.embedding.BaseEmbedding()
+    embedding.load(f'{model_path}/embedding.pickle')
 
-# label encode
-print('label encode --')
-if 'semeval' in config.stance_dataset:
-    stance_label = {'favor': 0, 'against': 1, 'none': 2}
-elif 'fnc' in config.stance_dataset:
-    stance_label = {'agree': 0, 'disagree': 1, 'discuss': 2, 'unrelated': 3}
+    # initialize device
+    device = torch.device('cpu')
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
 
-data_df['label_encode'] = data_df['label'].apply(
-    lambda label: stance_label[label])
+    # load data
+    data_df = util.data.load_dataset(dataset='semeval2016_test')
 
-# load lexicon
-lexicons = util.data.load_lexicon(lexicon=config.lexicon_file)
+    # content encode
+    data_df['target_encode'] = \
+        tokenizer.encode(data_df['target'].tolist())
+    data_df['claim_encode'] = \
+        tokenizer.encode(data_df['claim'].tolist())
 
-# build lexicon dictionary
-tokenizer.build_lexicon_dict(lexicons)
+    # label encode
+    if 'semeval' in config.stance_dataset:
+        stance_label = {'favor': 0, 'against': 1, 'none': 2}
+    elif 'fnc' in config.stance_dataset:
+        stance_label = {'agree': 0, 'disagree': 1,
+                        'discuss': 2, 'unrelated': 3}
 
-# encode content to lexicon vector
-data_df['claim_lexicon'] = \
-    tokenizer.encode_to_lexicon(data_df['claim_encode'].tolist())
+    data_df['label_encode'] = data_df['label'].apply(
+        lambda label: stance_label[label])
 
-# define dataset and dataloader
-dataset = util.data.SingleTaskDataset(
-    task_id=0,
-    target_encode=data_df['target_encode'],
-    claim_encode=data_df['claim_encode'],
-    claim_lexicon=data_df['claim_lexicon'],
-    label_encode=data_df['label_encode'])
-dataloader = DataLoader(
-    dataset=dataset,
-    batch_size=config.batch_size,
-    shuffle=False,
-    collate_fn=util.data.SingleTaskDataset.collate_fn)
+    # lexicon encode
+    data_df['claim_lexicon'] = \
+        tokenizer.encode_to_lexicon(data_df['claim_encode'].tolist())
 
-# define evaluate function
-def evaluate(model, batch_iterator):
-    total_loss, total_lexicon_loss = 0.0, 0.0
-    all_label_y, all_pred_y = [], []
+    # construct dataset and dataloader
+    dataset = util.data.SingleTaskDataset(
+        task_id=0,
+        target_name=data_df['target'],
+        target_encode=data_df['target_encode'],
+        claim_encode=data_df['claim_encode'],
+        claim_lexicon=data_df['claim_lexicon'],
+        label_encode=data_df['label_encode'])
+    dataloader = DataLoader(
+        dataset=dataset,
+        batch_size=config.batch_size,
+        shuffle=False,
+        collate_fn=util.data.SingleTaskDataset.collate_fn)
 
-    model.eval()
-    with torch.no_grad():
-        for task_id, x1, x2, lexicon, y in batch_iterator:
-            # fed data into model
-            x1 = x1.to(device)
-            x2 = x2.to(device)
-            lexicon = lexicon.to(device)
-            y = y.to(device)
-            pred_y, attn_weight = model(task_id, x1, x2)
+    # load model
+    model = util.model.BaseModel(config=config,
+                                 num_embeddings=embedding.get_num_embeddings(),
+                                 padding_idx=tokenizer.pad_token_id,
+                                 embedding_weight=embedding.vector)
+    model.load_state_dict(
+        torch.load(f'{model_path}/model_{epoch}.ckpt'))
+    model = model.to(device)
 
-            # evaluate loss
-            batch_loss, batch_lexicon_loss = \
-                util.loss.loss_function(task_id=task_id,
-                                        lexicon_vector=lexicon,
-                                        predict=pred_y,
-                                        target=y,
-                                        attn_weight=attn_weight,
-                                        nli_loss_weight=1.0,
-                                        lexicon_loss_weight=config.lexicon_loss_weight)
-            total_loss += batch_loss
-            total_lexicon_loss += batch_lexicon_loss
+    # evaluate
+    batch_iterator = tqdm(dataloader, total=len(dataloader),
+                          desc='evaluate test data', position=0)
+    (loss, lexicon_loss, score) = \
+        util.evaluate.stance_evaluate_function(device=device,
+                                               model=model,
+                                               config=config,
+                                               batch_iterator=batch_iterator)
 
-            all_label_y.extend(y.tolist())
-            all_pred_y.extend(torch.argmax(pred_y, axis=1).cpu().tolist())
+    # print loss and score
+    if 'semeval' in config.stance_dataset:
+        target_f1, macro_f1, micro_f1 = score
 
-    # evaluate loss
-    total_loss = total_loss / len(batch_iterator)
-    total_lexicon_loss = total_lexicon_loss / len(batch_iterator)
+        print(f'loss: {round(loss, 5)}, '
+              f'lexicon loss: {round(lexicon_loss, 5)}\n'
+              f'macro f1: {round(macro_f1, 5)}, '
+              f'micro f1: {round(micro_f1, 5)}\n'
+              f'target f1: {target_f1}')
+    elif 'fnc' in config.stance_dataset:
+        print(f'loss: {round(loss, 5)}, '
+              f'lexicon loss: {round(lexicon_loss, 5)}\n'
+              f'weighted score: {round(score, 5)}, ')
 
-    # get score and report
-    targets = (data_df['target']
-               if config.stance_dataset == 'semeval2016' else None)
-    scores = util.scorer.score_function(dataset=config.stance_dataset,
-                                        label_y=all_label_y,
-                                        pred_y=all_pred_y,
-                                        targets=targets)
+    # initialize tensorboard
+    writer = SummaryWriter(f'tensorboard/exp-{config.experiment_no}')
 
-    return total_loss, total_lexicon_loss, scores
+    # write loss and f1 to tensorboard
+    if 'semeval' in config.stance_dataset:
+        target_f1, macro_f1, micro_f1 = score
 
-# load model
-model = util.model.BaseModel(config=config,
-                             num_embeddings=embedding.get_num_embeddings(),
-                             padding_idx=tokenizer.pad_token_id,
-                             embedding_weight=embedding.vector)
-model.load_state_dict(
-    torch.load(f'{model_path}/model_{epoch}.ckpt'))
-model = model.to(device)
+        writer.add_scalar('Loss/test_stance', loss, epoch)
+        writer.add_scalar('Loss/test_lexicon', lexicon_loss, epoch)
 
-# evaluate
-model.eval()
+        writer.add_scalar('F1/test_macro', macro_f1, epoch)
+        writer.add_scalar('F1/test_micro', micro_f1, epoch)
 
-test_iterator = tqdm(dataloader, total=len(dataloader),
-                     desc='evaluate test set', position=0)
-loss, lexicon_loss, scores = evaluate(model, test_iterator)
+        writer.add_scalars('F1/test_target',
+                            {'atheism': target_f1[0],
+                             'climate': target_f1[1],
+                             'feminist': target_f1[2],
+                             'hillary': target_f1[3],
+                             'abortion': target_f1[4]}, epoch)
+    elif 'fnc' in config.stance_dataset:
+        writer.add_scalar('Loss/test_stance', loss, epoch)
+        writer.add_scalar('Loss/test_lexicon', lexicon_loss, epoch)
+        writer.add_scalar('F1/test_score', score, epoch)
 
-# print result
-if config.stance_dataset == 'semeval2016':
-    _, _, _, _, target_f1, macro_f1, micro_f1 = scores
+    writer.close()
 
-    print(f'\nexperiment {experiment_no}: {epoch}-epoch\n'
-          f'dataset: {config.stance_dataset}\n'
-          f'loss: {round(loss.item(), 5)}, '
-          f'lexicon loss: {round(lexicon_loss.item(), 5)}\n'
-          f'target f1: {target_f1}\n'
-          f'macro f1: {macro_f1}, micro f1: {micro_f1}\n')
+    # release GPU memory
+    torch.cuda.empty_cache()
 
-elif config.stance_dataset == 'fnc-1':
-    _, precision, recall, f1 = scores  # pylint: disable=unbalanced-tuple-unpacking
+    return
 
-    print(f'\nexperiment {experiment_no}: {epoch}-epoch\n'
-          f'dataset: {config.stance_dataset}\n'
-          f'loss: {round(loss.item(), 5)}, '
-          f'lexicon loss: {round(lexicon_loss.item(), 5)}\n'
-          f'f1: {f1}\n')
+if __name__ == '__main__':
+    main()
